89fc54f78150fcee9898327fb6009485e00620d2
Antti Juustila <antti.juustila@gmail.com>
2023-06-17 09:57:24 +0300

    Added an example of usage to example subdir

---
+++ example/Sources/PINVerifierTool.swift
@@ -0,0 +1,35 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+import FinnishPIDVerifier
+
+@main
+struct PINVerifierTool: ParsableCommand {
+
+	@Argument var pid: String
+
+	mutating func run() throws {
+		let verifier = FinnishPIDVerifier.verify(pid: pid)
+		switch verifier.validity {
+			case .validPID:
+				print("PID is valid Finnish PID")
+			case .invalidPID:
+				print("PID is invalid Finnish PID")
+			case .testPID:
+				print("PID is valid test PID")
+		}
+		if verifier.isValid {
+			print("PID is valid and not a test PID")
+			print("PID is for a person born in \(verifier.dateString!)")
+			print("Full date is \(verifier.birthDay!.formatted(date: .complete, time: .omitted))")
+			print("Gender of the PID holder is \(verifier.genderString)")
+			print("Date elements: day: \(verifier.day!) month: \(verifier.month!) year: \(verifier.year!)")
+		} else {
+			print("PID is either test PID or invalid")
+		}
+	}
+}
