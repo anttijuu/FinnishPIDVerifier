cc951a80cf8debe7fa234d455f6dfd34242130f0
Antti Juustila <antti.juustila@gmail.com>
2024-06-14 09:06:46 +0300

    Lifted swift tools to 5.10 and enabled strict concurrency setting in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FinnishPIDVerifier"),
+            name: "FinnishPIDVerifier",
+				swiftSettings: [
+					.enableExperimentalFeature("StrictConcurrency")
+				]
+		  ),
         .testTarget(
             name: "FinnishPIDVerifierTests",
             dependencies: ["FinnishPIDVerifier"]),
